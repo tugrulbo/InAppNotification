9bc6ac555577ea548920994c349a2340d689b31a
tugrulb <tugrulbo@gmail.com>
2021-07-29 20:15:25 +0300

    Initial commit

---
+++ app/src/main/java/com/tugrulbo/inappnotification/AfterActivity.kt
@@ -0,0 +1,15 @@
+package com.tugrulbo.inappnotification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class AfterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_after)
+    }
+
+
+}
